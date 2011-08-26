d31528fe1331b92e8bf7c30438605f8e6d620159
Michael Forrest <michael.forrest@gmail.com>
2011-08-26 12:54:06 +0100

    initial commit

---
+++ last.fm-spotify.js
@@ -0,0 +1,20 @@
+String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, "");
+}
+
+$(function() {
+    $(".subjectCell").each(function(index, cell) {
+        var $cell = $(cell);
+        var songName = $cell.text().trim();
+        var query = 'http://ws.spotify.com/search/1/track.json?q=' + songName;
+        $.get(query, function(data) {
+            if (data.tracks.length > 0) {
+                var track = data.tracks[0];
+                $cell.after('<td class="spotify"><a href="' + track.href + '" title="Play in Spotify"><img src="' + chrome.extension.getURL("spotify.png") + '"/></a></td>');
+            }
+        });
+
+    });
+});
+
+
